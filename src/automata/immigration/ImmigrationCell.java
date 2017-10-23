b3142afcb296749edeb59ef91d2b739f3c5b72e5
Baptiste <baptiste.rigondaud@ensimag.fr>
2017-10-23 15:34:51 +0200

    conflict resolving: new method ImmigrationCell

--- src/automata/immigration/ImmigrationCell.java
+++ src/automata/immigration/ImmigrationCell.java
@@ -15,66 +15,43 @@ import java.util.concurrent.ThreadLocalRandom;
  */
 public class ImmigrationCell extends Cell {
 
-    public ImmigrationCell(Grid grid, int i, int j, List<State> states) {
-        super(grid, i, j, states);
-    }
+	public ImmigrationCell(Grid grid, int i, int j, List<State> states) {
+		super(grid, i, j, states);
+	}
 
-<<<<<<< HEAD
 	@Override
 	protected final State initState() {
+		if (states.size() == 1) {
+			return states.get(0);
+		}
+
 		int randomIdState = ThreadLocalRandom.current().nextInt(0, states.size());
 		return states.get(randomIdState);
 	}
 
 	@Override
 	protected State nextState() {
-		int neighbourCounter = 0;
 		// If there's only one state: return the current state
 		if (states.size() == 1)
 			return this.state;
-		for (Cell neighbour : this.neighbours) {
-			if (neighbour.getState().getID() == (this.getState().getID() + 1)%(states.size()-1))
-				neighbourCounter++;
-		}
-		if (neighbourCounter >= 3)
-			return states.get((this.getState().getID() + 1)%(states.size()-1));
-		else
-			return this.state;
-	}
-=======
-    @Override
-    protected final State initState() {
-        if (states.size() == 1) {
-            return states.get(0);
-        }
-        
-        int randomIdState = ThreadLocalRandom.current().nextInt(0, states.size());
-        return states.get(randomIdState);
-    }
->>>>>>> develop
 
-    @Override
-    protected State nextState() {        
-        // If there's only one state: return the current state
-        if (states.size() == 1)
-            return this.state;
-        
-        int followingID = (this.getState().getID() + 1) % states.size();
-        State followingState = states.get(followingID);
-        
-        int neighbourCounter = this.countNeighbours(followingState);
-        return (neighbourCounter >= 3) ? followingState : this.state;
-        
-//        for (Cell neighbour : this.neighbours) {
-//            if (neighbour.getState().getID() == (this.getState().getID() + 1) % states.size()) {
-//                neighbourCounter++;
-//            }
-//        }
-//        
-//        if (neighbourCounter >= 3) {
-//            return states.get((this.getState().getID() + 1) % states.size());
-//        } else {
-//            return this.state;
-//        }
-    }
+		int followingID = (this.getState().getID() + 1) % states.size();
+		State followingState = states.get(followingID);
+
+		int neighbourCounter = this.countNeighbours(followingState);
+		return (neighbourCounter >= 3) ? followingState : this.state;
+
+		// for (Cell neighbour : this.neighbours) {
+		// if (neighbour.getState().getID() == (this.getState().getID() + 1) %
+		// states.size()) {
+		// neighbourCounter++;
+		// }
+		// }
+		//
+		// if (neighbourCounter >= 3) {
+		// return states.get((this.getState().getID() + 1) % states.size());
+		// } else {
+		// return this.state;
+		// }
+	}
 }
--- src/test/TestImmigrationGame.java
+++ src/test/TestImmigrationGame.java
@@ -18,7 +18,6 @@ public class TestImmigrationGame {
 	 *            the command line arguments
 	 */
 	public static void main(String[] args) {
-
 		ImmigrationAutomaton immigrationGame = new ImmigrationAutomaton(50, 50, 10, 5);
 		immigrationGame.simulate();
 	}
