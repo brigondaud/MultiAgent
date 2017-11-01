45fd22131faf1a787265542d11e4a77e72dac85a
Baptiste <baptiste.rigondaud@ensimag.fr>
2017-11-01 10:01:05 +0100

    tests for the event and event manager

---
+++ src/event/MessageEvent.java
@@ -0,0 +1,16 @@
+package event;
+
+import event.Event;
+
+public class MessageEvent extends Event {
+	private String message;
+
+	public MessageEvent(int date, String message) {
+		super(date);
+		this.message = message;
+	}
+
+	public void execute() {
+		System.out.println(this.getDate() + this.message);
+	}
+}
